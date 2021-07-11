49acc4b1f91aec3dd56c927e663b1a1bedf59057
ajanthan <ajanthan.kt@gmail.com>
2021-07-11 11:45:54 +1000

    Using shouldly

--- Test/ClassCalculatorTests.cs
+++ Test/ClassCalculatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using Calculator4NUnit;
 using NSubstitute;
 using NUnit.Framework;
+using Shouldly;
 
 namespace Test
 {
@@ -12,7 +13,7 @@ namespace Test
         public CalculatorTests()
         {
            sut = new Calculator();
-            calculator = Substitute.For<ICalculator>();
+           calculator = Substitute.For<ICalculator>();
         }
 
         [Test]
@@ -21,14 +22,13 @@ namespace Test
             //Arrange
             int number1 = 1;
             int number2 = 2;
-
             calculator.AddTwoNumbers(number1,number2).Returns(3);
 
             //Act
             int outputNumbers = sut.AddTwoNumbers(number1, number2);
 
             //Assert
-            Assert.AreEqual(calculator.AddTwoNumbers(number1, number2), outputNumbers);
+            outputNumbers.ShouldBe(calculator.AddTwoNumbers(number1, number2));
         }
 
         [Test]
@@ -37,15 +37,13 @@ namespace Test
             //Arrange
             int number1 = 5;
             int number2 = 3;
-
-            int resultNumber = 2;
-            calculator.SubstractTwoNumbers(number1, number2).Returns(8);
-
+            calculator.SubstractTwoNumbers(number1, number2).Returns(2);
 
             //Act
             var outputNumber = sut.SubstractTwoNumbers(number1, number2);
 
-            Assert.AreEqual(resultNumber, outputNumber);
+            //Assert
+            outputNumber.ShouldBe(calculator.SubstractTwoNumbers(number1, number2));
         }
 
         [Test]
@@ -54,13 +52,13 @@ namespace Test
             //Arrange
             int number1 = 5;
             int number2 = 3;
-
             calculator.MultiplyTwoNumbers(number1, number2).Returns(15);
 
             //Act
             var outputNumber = sut.MultiplyTwoNumbers(number1, number2);
 
-            Assert.AreEqual(calculator.MultiplyTwoNumbers(number1, number2), outputNumber);
+            //Assert
+            outputNumber.ShouldBe(calculator.MultiplyTwoNumbers(number1, number2));
         }
 
         [Test]
@@ -68,15 +66,14 @@ namespace Test
         {
             //Arrange
             int number1 = 15;
-            int number2 = 3;
-
-            int resultNumber = 5;
+            int number2 = 3;          
+            calculator.DivideTwoNumbers(number1, number2).Returns(5);
 
             //Act
             var outputNumber = sut.DivideTwoNumbers(number1, number2);
-            calculator.DivideTwoNumbers(number1, number2).Returns(3);
-
-            Assert.AreEqual(resultNumber, outputNumber);
+           
+            //Assert
+            outputNumber.ShouldBe(calculator.DivideTwoNumbers(number1, number2));
         }
     }
 }
